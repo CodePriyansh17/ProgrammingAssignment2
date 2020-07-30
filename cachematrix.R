21ea2948355058b9bb9b7a35fcafd7d9a74a7873
CodePriyansh17 <68993708+CodePriyansh17@users.noreply.github.com>
2020-07-30 15:34:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,8 @@
       inv <<- NULL
     }
     get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
